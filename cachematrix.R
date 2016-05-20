4349b6eafa0530495c093962ed0f8ea4dc21592b
careysuehs <carey.suehs@chu-nimes.fr>
2016-05-21 00:52:04 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions store / retrieve the inverse of a matrix. If the inverse has
+#not already been computed, the function cacheSolve computes it.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function creates an object with properties a and i + four "methods"
+        #sett    if called, assign new value b to property a
+        #gett    if called, return a
+        #setinvrs    if called, assign argument to i
+        #getinvrs    if called, return i
 
+makeCacheMatrix <- function(a = matrix()){
+        i <- NULL
+        sett <- function(b) {   #if called, assign new value b to property a
+                a <<- b
+                i <<- NULL
+        }
+        gett <- function () a     #if called, return a
+        setinvrs <- function(solve) i <<- solve  #in called, set argument as i
+        getinvrs <- function() i                  #if called, return i
+        list (sett = sett,            #output = list of 4 fxns
+              gett = gett, 
+              setinvrs = setinvrs, 
+              getinvrs = getinvrs)
 }
 
 
-## Write a short comment describing this function
+## This function treats an object (created above). It first calls the getinvrs
+#method and checks if the value has been computed already or not. If not, it 
+#computes and returns the value.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) {
+        i <- a$getinvrs()        #will be null if never done before
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        dataa <- a$gett()
+        i <- solve(dataa, ...)
+        i
 }
